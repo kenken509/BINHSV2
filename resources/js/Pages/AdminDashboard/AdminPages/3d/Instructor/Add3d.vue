50f20937a551ca0ea1cd56a2e5b4aa0e28d44209
kenken509 <ariesllesis050382@gmail.com>
2024-01-21 08:38:00 +0800

    3d management valid

--- resources/js/Pages/AdminDashboard/AdminPages/3d/Instructor/Add3d.vue
+++ resources/js/Pages/AdminDashboard/AdminPages/3d/Instructor/Add3d.vue
@@ -9,7 +9,7 @@
                             
             </div>
         </div>
-        <span class="text-red-500 text-[30px]">TO DO: VALIDATE THE DATA BEFORE SUBMISSION</span>
+        <span class="text-red-500 text-[30px]">TO DO: PROCESS THE SUBMISSION ON BACKEND</span>
         <div  class="col-span-12 mt-8">
             <form @submit.prevent="submit">
                 <div class="flex flex-col">
@@ -30,18 +30,23 @@
                 <input type="file" accept="image/*" hidden id="imageAttachment" @input="Add3dImage"/>
                 
                 <div class="mb-4">
-                    <label for="imageAttachment" class="file-input-label bg-gray-300 px-4 py-2 rounded-md cursor-pointer w-[200px]">
-                        Select image file... 
-                    </label>
+                    <div class="flex items-center">
+                        <label for="imageAttachment" class="file-input-label bg-gray-300 px-4 py-2 rounded-md cursor-pointer w-[200px]">
+                            Select image file... 
+                        </label>
+                        
+                        <h1 class="ml-2">{{ threeDImageFileName }}</h1> 
+                        <InputError :error="imageValidator" />
+                    </div>
+                    
                     <div v-if="imageUrl" class="mx-2 mt-2 p-1 bg-gray-200  inline-block relative  border border-gray-300  rounded-md " >
-                        <h1 class="">{{ threeDImageFileName }}</h1> 
-                        <!-- <div class="absolute right-[-7px] top-[-7px] hover:right-[-9px] hover:top-[-7px] cursor-pointer">
-                            <i class="pi pi-times-circle text-red-700 cursor-pointer hover:text-[20px]" @click="deleteImage" ></i>
-                        </div> -->
+                        
+                        <div v-if="imageUrl" class="flex justify-center items-center border border-gray-300 rounded-md p-2 shadow-md mt-2" >
+                            <img :src="imageUrl" alt="Error" class="w-[50%] h-[50%] rounded-md relative"/>
+                        </div>
+                        
                     </div>
-                    <div v-if="imageUrl" class="flex justify-center items-center border border-gray-300 rounded-md p-2 shadow-md mt-2" >
-                        <img :src="imageUrl" alt="Error" class="w-[50%] h-[50%] rounded-md relative"/>
-                    </div>
+                    
                 </div>
                 <div class="my-2">
                     <hr class="border border-1 border-gray-400">
@@ -52,11 +57,13 @@
                         <label for="3dFile" class="file-input-label bg-gray-300 px-4 py-2 rounded-md cursor-pointer">
                             Select a file... 
                         </label>
+                        <InputError :error="threeDValidator" />
                         <div v-if="threeDFilename" class=" ml-2 flex items-center">
                             <i class="fa-solid fa-file-zipper fa-2x text-red-700"></i>
                             <span class="text-red-700 ml-2"> {{ threeDFilename }}</span>
                         </div>
                     </div>
+                    
                     <input type="file" accept=".zip, .rar" hidden id="3dFile" @input="Add3dFile"/>
                 </div>
                 <div class="my-2">
@@ -75,6 +82,7 @@
 <script setup>
 import DashboardLayout from '../../../Layout/DashboardLayout.vue'
 import { useForm, usePage } from '@inertiajs/vue3'
+import InputError from '../../../../GlobalComponent/InputError.vue'
 import {ref} from 'vue'
 
 const user = usePage().props.user;
@@ -87,25 +95,79 @@ const form = useForm({
 
 const imageUrl = ref(null);
 const threeDImageFileName = ref(null);
-
-
+const imageValidator = ref(null);
+const imageIsValid = ref(false);
 const Add3dImage = (event)=>{
-
-    threeDImageFileName.value = event.target.files[0].name;
-
-    form.image = event.target.files[0];
     
-    imageUrl.value = URL.createObjectURL(event.target.files[0]);
+    //console.log(event.target.files[0].size)
+    if(event.target.files[0].type !== 'image/jpeg' || event.target.files[0].size > 3000000)
+    {
+        imageIsValid.value = false
+        imageValidator.value = 'File should be of type "jpeg, jpg" with maximum size of 3mb';
+        threeDImageFileName.value = null;
+        imageUrl.value = null;
+    }
+    else
+    {
+        imageIsValid.value = true
+        imageValidator.value = null;
+
+        threeDImageFileName.value = event.target.files[0].name;
+
+        form.image = event.target.files[0];
+        
+        imageUrl.value = URL.createObjectURL(event.target.files[0]);
+    }
+
+    
 }
 
 const threeDFilename = ref(null);
+const threeDValidator = ref(null);
+const threeDIsValid = ref(false);
 const Add3dFile = (event) =>{
-    threeDFilename.value = event.target.files[0].name;
 
-    form.threeDFile = event.target.files[0];
+    if(!(
+        (event.target.files[0].type === 'application/x-zip-compressed' ||
+         event.target.files[0].type === 'application/x-compressed') &&
+        event.target.files[0].size <= 50000000
+    ))
+    {
+        threeDFilename.value = null
+        threeDValidator.value = 'File should be of type ".zip, .rar" with maximum size of 50mb';
+    }
+    else
+    {
+        threeDIsValid.value = true;
+        threeDValidator.value = ''
+        threeDFilename.value = event.target.files[0].name;
+
+        form.threeDFile = event.target.files[0];
+    }
+    
 }
 
 const submit = ()=>{
-    console.log(form);
+
+    if(form.image === null)
+    {
+        imageValidator.value = 'Image is required.'
+    }
+    else if(form.threeDFile === null)
+    {
+        threeDValidator.value = '3d file is requred.'
+    }
+    else
+    {
+        if(imageIsValid.value && threeDIsValid.value)
+        {
+            alert('submit allowed')
+        }
+        else
+        {
+            alert('submit not allowed')
+        }
+    }
+    
 }
 </script>
